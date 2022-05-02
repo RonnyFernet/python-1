e85a4b81af20aff963f6ebc3391503415fd0aa7c
Daniel Boman <46175007+trybefore@users.noreply.github.com>
2022-05-02 13:20:44 +0200

    Update funksjoner.py

--- funksjoner.py
+++ funksjoner.py
@@ -3,3 +3,10 @@ def multiply(a,b):
 
 def divide(a,b):
 	return a / b
+
+
+a = float(input("a: "))
+b = float(input("b: "))
+
+print(f"{a}*{b} = {round(multiply(a, b), 2)}")
+print(f"{a}/{b} = {round(divide(a, b), 2)}")
